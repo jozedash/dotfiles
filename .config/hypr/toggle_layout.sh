123f8217bed6049acd3edcac2f57d9fefa9b0637
jozedash <josiah@thekemps.co.uk>
2025-12-07 11:08:52 +0000

    Revised and improved hyprland config

--- .config/hypr/toggle_layout.sh
+++ .config/hypr/toggle_layout.sh
@@ -3,24 +3,37 @@
 STATEFILE="$HOME/.config/hypr/layout_state"
 set -x
 # Add layouts here
-layouts=("gaming" "docked" "extend")
+LAPTOP=0
+DOCKED=1
+EXTEND=2
+GAMING=3
+MODE_COUNT=4 # Used to ensure the loop resets
 
 if [ ! -f "$STATEFILE" ]; then
-    # init to 0
-    echo "0" > "$STATEFILE"
+    # init to laptop only
+    echo $LAPTOP > "$STATEFILE"
 fi
 
-state=$(cat "$STATEFILE")
+# Read existing state
+STATE=$(cat "$STATEFILE")
 
-if [ "$1" == "toggle" ]; then
-    state="$((state + 1))"
-
-    if [ $state -ge ${#layouts[@]} ]; then
-        state=0
+# Default back to laptop if no external monitor
+if ! hyprctl monitors | grep -q "Monitor DP-2"; then
+    if [ $STATE -ne $LAPTOP ]; then
+        STATE=$LAPTOP
     fi
+else
+    # Toggle to next state if requested
+    if [ "$1" == "toggle" ]; then
+        STATE="$((STATE + 1))"
+
+        # Reset to default
+        if [ $STATE -ge $MODE_COUNT ]; then
+            STATE=$LAPTOP
+        fi
 fi
 
-if [ ${layouts[$state]} = "docked" ]; then
+if [ $STATE -eq $DOCKED ]; then
     # Switch to docked
     hyprctl keyword monitor "eDP-1,disable"
     hyprctl keyword monitor "DP-2,3840x2160@60,0x0,1"
@@ -30,7 +43,7 @@ if [ ${layouts[$state]} = "docked" ]; then
     hyprctl dispatch moveworkspacetomonitor 4 DP-2
     hyprctl dispatch moveworkspacetomonitor 5 DP-2
     hyprctl dispatch moveworkspacetomonitor 6 DP-2
-elif [ ${layouts[$state]} = "gaming" ]; then
+elif [ $STATE -eq $GAMING ]; then
     # Lower resolution
     hyprctl keyword monitor "eDP-1,disable"
     hyprctl keyword monitor "DP-2,1600x900@60,0x0,1"
@@ -40,7 +53,7 @@ elif [ ${layouts[$state]} = "gaming" ]; then
     hyprctl dispatch moveworkspacetomonitor 4 DP-2
     hyprctl dispatch moveworkspacetomonitor 5 DP-2
     hyprctl dispatch moveworkspacetomonitor 6 DP-2
-elif [ ${layouts[$state]} = "extend" ]; then
+elif [ $STATE -ne $EXTEND ]; then
     # Extend
     hyprctl keyword monitor "eDP-1,1920x1080@60,3840x1080,1"
     hyprctl keyword monitor "DP-2,3840x2160@60,0x0,1"
@@ -50,6 +63,16 @@ elif [ ${layouts[$state]} = "extend" ]; then
     hyprctl dispatch moveworkspacetomonitor 4 eDP-1
     hyprctl dispatch moveworkspacetomonitor 5 DP-2
     hyprctl dispatch moveworkspacetomonitor 6 eDP-1
+elif [ $STATE -ne $LAPTOP ]; then
+    # Laptop only
+    hyprctl keyword monitor "eDP-1,1920x1080@60,0x0,1"
+    hyprctl keyword monitor "DP-2,disable"
+    hyprctl dispatch moveworkspacetomonitor 1 eDP-1
+    hyprctl dispatch moveworkspacetomonitor 2 eDP-1
+    hyprctl dispatch moveworkspacetomonitor 3 eDP-1
+    hyprctl dispatch moveworkspacetomonitor 4 eDP-1
+    hyprctl dispatch moveworkspacetomonitor 5 eDP-1
+    hyprctl dispatch moveworkspacetomonitor 6 eDP-1
 fi
 
-echo $state > "$STATEFILE"
+echo $STATE > "$STATEFILE"
